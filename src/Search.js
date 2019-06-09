93e1960c1c83e5d86a062aaad38d13e111a5bbb7
AppShore <git@appshore.com>
2019-06-09 09:00:55 +0100

    search component

---
+++ src/Search.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ''
+    }
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  async fetchResults({ search }) {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/api/books/${search}`,
+        {
+          mode: 'cors',
+          headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json'
+          }
+        }
+      );
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return this.props.onSearch(await response.json());
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      e.stopPropagation();
+      this.handleSubmit(e);
+    }
+  }
+
+  handleSubmit(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    }, () => this.fetchResults({ search: this.state.searchValue }))
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          name="searchValue"
+          type="text"
+          className="searchInput"
+          onKeyDown={this.handleKeyDown}
+          defaultValue={this.state.searchValue}
+          placeholder='Search by OLID or Title'
+        />
+      </div>
+    )
+  }
+}
+
+
+export default Search;
